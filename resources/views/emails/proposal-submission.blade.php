27d3aca8468fdc973562c703f94040d59696338d
oxytoxin <oxytoxinsgrace@gmail.com>
2023-07-08 08:00:38 +0800

    inline attachments as links

--- resources/views/emails/proposal-submission.blade.php
+++ resources/views/emails/proposal-submission.blade.php
@@ -1,17 +1,14 @@
 <x-mail::message>
-    # New Proposal Submission
+# New Proposal Submission
 
-    Startup Name: {{ $proposal->startup_name }}
+Startup Name: {{ $proposal->startup_name }}
 
-    Members:
-    @foreach ($proposal->members as $member)
-        {{ $member }}
-    @endforeach
+Members:
+@foreach ($proposal->members as $member)
+    {{ $member }}
+@endforeach
 
-    Logo:
-    <x-mail::button :url="$proposal->getFirstMedia('logo')->getUrl()">Logo</x-mail::button>
-
-    Proposal:
-    <x-mail::button :url="$proposal->getFirstMedia('proposal')->getUrl()">Proposal</x-mail::button>
+<x-mail::button :url="$proposal->getFirstMedia('logo')->getUrl()">View Logo</x-mail::button>
+<x-mail::button :url="$proposal->getFirstMedia('proposal')->getUrl()">View Proposal</x-mail::button>
 
 </x-mail::message>
